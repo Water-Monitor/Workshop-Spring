b674090eafcdce3c2f1a88943fbdbcced4ac88db
tjalk20sd <tjalk20sd@gmail.com>
2021-12-03 09:16:27 +0100

    init projecty

---
+++ workshop-spring/src/test/java/watermonitor/workshopspring/WorkshopSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package watermonitor.workshopspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
