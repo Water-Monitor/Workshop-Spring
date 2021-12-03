58c83d04f9430fb7bb3612ff7f8229180921ae1a
tjalk20sd <tjalk20sd@gmail.com>
2021-12-03 09:50:13 +0100

    init

---
+++ workshop-spring/src/main/java/watermonitor/workshopspring/controllers/TipController.java
@@ -0,0 +1,10 @@
+package watermonitor.workshopspring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Controller
+public class TipController {
+}
+
